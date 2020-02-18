77dfcfc21b9e55052df1ce9fa7a5402a08fac2af
Oladele Joshua <jurshsmith@gmail.com>
2020-02-18 13:13:51 +0100

    single dockerizing

--- phollery-api/src/main.ts
+++ phollery-api/src/main.ts
@@ -4,7 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   console.log(NestFactory)
   const app = await NestFactory.create(AppModule);
+  process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
   app.enableCors();
-  await app.listen(3000);
+  const port = process.env.PORT || '3000';
+  await app.listen(port);
 }
 bootstrap();
